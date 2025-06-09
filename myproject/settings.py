504cc6e057dd186da292a950cb786d6767a1a6a6
adhithyants <adhithyan1883@gmail.com>
2025-06-09 19:44:47 +0530

    Switch database from SQLite to PostgreSQL for Railway

--- myproject/settings.py
+++ myproject/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -19,14 +19,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@@cl#kh@$!y=wmh_u*&xp-*@%*=b3x_k!vzg@pa-q3_x*3@fo&'
+SECRET_KEY = os.getenv('SECRET_KEY', 'fallback-secret')
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
@@ -70,7 +67,7 @@ CORS_ALLOWED_ORIGINS = [
     'http://0.0.0.0:8000'# Adjust for your Flutter app
 ]
 CORS_ALLOW_METHODS = ['GET', 'POST', 'OPTIONS']
-ALLOWED_HOSTS = ['*','192.168.39.21','127.0.0.1']
+
 
 ROOT_URLCONF = 'myproject.urls'
 
@@ -95,11 +92,12 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.getenv("DATABASE_URL")
+    )
 }
 
 
